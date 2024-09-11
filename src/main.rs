186c52969e2cc145e18993adc4d4b6f6fb71e69f
korinlu <korinlu1@fel.cvut.cz>
2024-09-11 21:53:54 +0900

    Hello World!

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,21 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
 // Ensure that the Rust compiler really outputs a function with the name _start.
-// Without the compiler would generate some cryptic _ZN3blog_os4_start7hb173fedf945531caE
+// Without the compiler would generate some cryptic _ZN3blog_os4_start7hb173feds945531caE
 // to give every function a unique name.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
+    let vga_buffer = 0xb8000 as *mut u8; // Address of VGA buffer
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0x0f; // Color attribute
+        }
+    }
     loop {}
 }
 
