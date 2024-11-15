812660b494326496074be618a7684d2c0805019c
korinlu <korinlu1@fel.cvut.cz>
2024-11-15 12:24:35 +0900

    Added test framework

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,36 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
+#![feature(custom_test_frameworks)]
+#![test_runner(x86_rust_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
+use x86_rust_os::{print, println};
 use core::panic::PanicInfo;
-use crate::vga_buffer::{Color, ScreenWriter};
 
-mod vga_buffer;
-
-// Ensure that the Rust compiler really outputs a function with the name _start.
-// Without the compiler would generate some cryptic _ZN3blog_os4_start7hb173feds945531caE
+// #[no_mangle] ensures that the Rust compiler really outputs a function with the name _start.
+// Without it the compiler would generate some cryptic name "_ZN3blog_os4_start7hb173feds945531caE"
 // to give every function a unique name.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-
     println!("Hello World!");
     println!("My favorite numbers are {} and {}", 3, 13);
-    panic!("some panic");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
 /// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 } // Diverging function "-> !" never returns
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    x86_rust_os::test_panic_handler(info)
+}
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -97,7 +97,7 @@ impl ScreenWriter {
         }
     }
 
-    pub fn write_string(&mut self, string: &str) {
+    fn write_string(&mut self, string: &str) {
         for byte in string.bytes() {
             match byte {
                 // printable ASCII byte or newline
@@ -139,4 +139,26 @@ lazy_static! {
         color_code: ColorCode::new(Color::White, Color::Black),
         buffer: unsafe { &mut *(0xb8000 as *mut ScreenBuffer) },
     });
+}
+
+#[test_case]
+fn test_println_simple() {
+    println!("test_println_simple output");
+}
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    let s = "Some test string that fits on a single line";
+    println!("{}", s);
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
 }
